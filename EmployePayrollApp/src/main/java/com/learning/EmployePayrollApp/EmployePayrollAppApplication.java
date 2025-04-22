55feb3f8ff589705cc019b5d07cff59f8c96dcdb
simrandhiman <simrandhiman3121@gmail.com>
2025-04-22 21:26:50 +0530

    [Simran] added EmployeePayrollApp

---
+++ EmployePayrollApp/src/main/java/com/learning/EmployePayrollApp/EmployePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.EmployePayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployePayrollAppApplication.class, args);
+	}
+
+}
